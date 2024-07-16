abfed32dc9f7fea476f369d28ba17187f288544c
risinget <tenorioquirozeduar7@gmail.com>
2024-07-16 07:59:15 -0400

    chore: Add Fabric repositories and git attributes
    This commit adds the necessary configuration files for the Fabric mod development environment. The `settings.gradle` file includes the Fabric Maven repository, and the `.gitattributes` file sets the line endings for different script files.

---
+++ src/main/java/risinget/commander/Commander.java
@@ -0,0 +1,29 @@
+package risinget.commander;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+
+//
+
+
+//
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mojang.brigadier.context.CommandContext;
+
+public class Commander implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger("commander");
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+
+    }
+
+    
+}
