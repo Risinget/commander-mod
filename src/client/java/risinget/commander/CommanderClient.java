b91b03b7c2026dd7cbb317af87af81098993910b
risinget <tenorioquirozeduar7@gmail.com>
2024-07-16 09:00:10 -0400

    chore: Update CommanderClient and Commander classes

--- src/client/java/risinget/commander/CommanderClient.java
+++ src/client/java/risinget/commander/CommanderClient.java
@@ -2,6 +2,7 @@ package risinget.commander;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 
@@ -12,6 +13,14 @@ public class CommanderClient implements ClientModInitializer {
 		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
 			dispatcher.register(ClientCommandManager.literal("clienttater").executes(context -> {
 				context.getSource().sendFeedback(Text.literal("Called /clienttater with no arguments."));
+				return 1;
+			}));
+		});
+
+
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(ClientCommandManager.literal("prueba_mundo").executes(context -> {
+				context.getSource().sendFeedback(Text.literal("hola mundo!!!").formatted(Formatting.RED));
 				return 1;
 			}));
 		});
